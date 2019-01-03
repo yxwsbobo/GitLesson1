f672b1ee19805a25e0ca4b6973b50b782629a1ed
yxwsbobo <yxwsbobo@gmail.com>
2019-01-03 14:23:44 +0800

    commit direct2

--- Lession01/main.cpp
+++ Lession01/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
 
+template<typename... T>
+decltype(auto) Add(T&&... args)
+{
+	return (args + ...);
+}
+
 int main()
 {
 	auto SayHello = std::string("Hello World!");
 	std::cout<< SayHello <<std::endl;
-	
+
+	std::cout <<"Add num is :"<<Add(3,5,2,6,4,8)<<std::endl;	
 	return 0;
 }
