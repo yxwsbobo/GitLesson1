342d669edf3b04ad7104651fa2b04418a4881b30
yxwsbobo <yxwsbobo@gmail.com>
2019-01-08 18:59:03 +0800

    Print stars program

--- main.cpp
+++ main.cpp
@@ -1,34 +1,9 @@
 #include <iostream>
 #include <string>
 
-template<typename... T>
-decltype(auto) Add(T&&... args)
-{
-	return (args + ...);
-}
 
-template<typename T>
-T mul(T a, T b)
-{
-	return a * b;
-}
-
-void Temp()
-{
-	std::cout<<"in Temp"<<std::endl;
-
-}
-
-void NewTemp()
-{
-	std::cout << "in NewTemp"<<std::endl;
-}
 int main()
 {
-	auto SayHello = std::string("Hello World!");
-	std::cout<< SayHello <<std::endl;
-
-	std::cout <<"Add num is :"<<Add(3,5,2,6,4,8)<<std::endl;	
-	//Add some comment
+	std::cout<<"print Start Program"<<std::endl;
 	return 0;
 }
