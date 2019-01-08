bfe6189dbea9f69c4eb69c628869fb8ccad08733
yxwsbobo <yxwsbobo@gmail.com>
2019-01-08 19:11:11 +0800

    add ShapeGenerator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 #include <string>
 
+class ShapeGenerator
+{
+public:
+	ShapeGenerator(int totalLines):TotalLines{totalLines}{}
+	~ShapreGenerator()= default;
+
+	void print()
+	{
+		for(int i=0; i!= TotalLines; ++i)
+		{
+			RepeatPrint(" ",ComputeCurrentSpace(i+1);
+			RepeatPrint("*",ComputeCurrentStars(i+1);
+			std::cout<<std::endl;
+		}
+	}
+
+private:
+	int ComputeCurrentSpace(int LineNumber)
+	{
+		return 0;
+	}
+
+	int ComputeCurrentStars(int LineNumbers)
+	{
+		return 0;
+	}
+
+private:
+	int TotalLines;
+}
+
 
 int main()
 {
