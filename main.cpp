6358dbd444c1571bc45d3155ea2768ed7c9b036a
yxwsbobo <yxwsbobo@gmail.com>
2019-01-08 19:17:58 +0800

    fix bugs

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <string>
 
+void RepeatPrint(std::string, uint32_t);
+
 class ShapeGenerator
 {
 public:
 	ShapeGenerator(int totalLines):TotalLines{totalLines}{}
-	~ShapreGenerator()= default;
+	~ShapeGenerator() = default;
 
 	void print()
 	{
 		for(int i=0; i!= TotalLines; ++i)
 		{
-			RepeatPrint(" ",ComputeCurrentSpace(i+1);
-			RepeatPrint("*",ComputeCurrentStars(i+1);
+			RepeatPrint(" ",ComputeCurrentSpace(i+1));
+			RepeatPrint("*",ComputeCurrentStars(i+1));
 			std::cout<<std::endl;
 		}
 	}
@@ -44,5 +46,9 @@ void RepeatPrint(std::string str, uint32_t times)
 int main()
 {
 	std::cout<<"print Start Program"<<std::endl;
+
+	ShapeGenerator sg{10};
+
+	sg.print();
 	return 0;
 }
